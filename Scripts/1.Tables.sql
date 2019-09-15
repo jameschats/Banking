ee8c4af632ad087ae44381296a754e8afa9f117e
jameschats30 <jameschats@gmail.com>
2019-09-16 01:29:43 +0530

    Banking-update

---
+++ Scripts/1.Tables.sql
@@ -0,0 +1,86 @@
+USE [Banking]
+GO
+
+/****** Object:  Table [dbo].[tblAccount]    Script Date: 15-09-2019 23:22:16 ******/
+DROP TABLE [dbo].[tblAccount]
+GO
+
+/****** Object:  Table [dbo].[tblAccount]    Script Date: 15-09-2019 23:22:16 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblAccount](
+	[AccountId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[MonthlyCreditLimit] [float] NULL,
+ CONSTRAINT [PK_tblAccount] PRIMARY KEY CLUSTERED 
+(
+	[AccountId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+/****** Object:  Table [dbo].[tblPayment]    Script Date: 15-09-2019 23:22:36 ******/
+DROP TABLE [dbo].[tblPayment]
+GO
+
+/****** Object:  Table [dbo].[tblPayment]    Script Date: 15-09-2019 23:22:36 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblPayment](
+	[TransactionId] [bigint] IDENTITY(1,1) NOT NULL,
+	[TransactionDate] [datetime] NOT NULL,
+	[AccountId] [int] NOT NULL,
+	[Amount] [float] NOT NULL,
+ CONSTRAINT [PK_tblPayment] PRIMARY KEY CLUSTERED 
+(
+	[TransactionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+USE [Banking]
+GO
+
+ALTER TABLE [dbo].[tblUser] DROP CONSTRAINT [DF_tblUser_Active]
+GO
+
+/****** Object:  Table [dbo].[tblUser]    Script Date: 15-09-2019 23:22:55 ******/
+DROP TABLE [dbo].[tblUser]
+GO
+
+/****** Object:  Table [dbo].[tblUser]    Script Date: 15-09-2019 23:22:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblUser](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](254) NOT NULL,
+	[PasswordHash] [varbinary](max) NULL,
+	[PasswordSalt] [varbinary](max) NULL,
+	[Active] [bit] NULL,
+ CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tblUser] ADD  CONSTRAINT [DF_tblUser_Active]  DEFAULT ((1)) FOR [Active]
+GO
+
+
+
---
+++ Scripts/2.Procedure.sql
@@ -0,0 +1,58 @@
+USE [Banking]
+GO
+
+/****** Object:  StoredProcedure [dbo].[prcGetPerformers]    Script Date: 15-09-2019 23:24:52 ******/
+DROP PROCEDURE [dbo].[prcGetPerformers]
+GO
+
+/****** Object:  StoredProcedure [dbo].[prcGetPerformers]    Script Date: 15-09-2019 23:24:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[prcGetPerformers](@FromDate      DATETIME, 
+                                 @ToDate        DATETIME, 
+                                 @SortColumn    VARCHAR(20), 
+                                 @SortDirection VARCHAR(5)) 
+--EXEC prcGetPerformers '2019-09-10','2019-09-16', 'amount', 'desc' 
+--EXEC prcGetPerformers '2019-09-10','2019-09-16', 'usage', 'desc' 
+AS 
+  BEGIN 
+      DECLARE @sql AS NVARCHAR(4000) 
+
+      SET @sql = 'SELECT CONVERT(nvarchar(max), [b].[AccountId]) AS [AccountId], [b].[Name],  
+					 CAST((SUM([a].[Amount]) / [b].[MonthlyCreditLimit]) * 100E0 AS DECIMAL(18,2)) AS Usage,
+					 CAST( SUM([a].[Amount]) AS DECIMAL(18,2)) AS [Amount], 
+					 CAST( [b].[MonthlyCreditLimit] AS DECIMAL(18,2)) AS MonthlyCreditLimit       
+				 FROM [tblPayment] AS [a]       
+				 INNER JOIN [tblAccount] AS [b] ON [a].[AccountId] = [b].[AccountId]  
+				  WHERE (convert(Datetime,[a].[TransactionDate]) >= ''' 
+                 + CONVERT(NVARCHAR(24), @FromDate, 121) 
+                 + ''')        AND (convert(Datetime,[a].[TransactionDate]) <= ''' 
+                 + CONVERT(NVARCHAR(24), @ToDate, 121) 
+                 + 
+      ''')        GROUP BY [b].[Name], [b].[AccountId], [b].[MonthlyCreditLimit]' 
+
+      IF @SortColumn = '' 
+          OR @SortColumn IS NULL 
+        SET @sql = @sql + ' ORDER BY Usage DESC' 
+
+      IF @SortColumn <> '' 
+         AND @SortColumn IS NOT NULL 
+        SET @sql = @sql + ' ORDER BY ' + @SortColumn 
+
+      IF @SortDirection <> '' 
+         AND @SortDirection IS NOT NULL 
+        SET @sql = @sql + ' ' + @SortDirection 
+
+      --PRINT @sql 
+
+      EXEC Sp_executesql 
+        @sql 
+  END 
+GO
+
+
